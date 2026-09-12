MT6897A/packages_modules_Bluetooth

Add L2CAP and A2DP offload coex mechanism for MTK
Co-authored-by: nift4 <nift4@protonmail.com>
Change-Id: I7047fa496f60843e664e4b34f12e1c27f492a4ba

system/bta/av/bta_av_aact.cc
@@ -80,6 +80,7 @@
 #include "stack/include/btm_log_history.h"
 #include "stack/include/btm_status.h"
 #include "stack/include/l2cap_interface.h"
+#include "stack/include/l2cap_av_interface.h"
 #include "storage/config_keys.h"
 
 using namespace bluetooth;
@@ -3060,6 +3061,11 @@ static void offload_vendor_callback(tBTM_VSC_CMPL* param) {
       case VS_HCI_A2DP_OFFLOAD_STOP:
       case VS_HCI_A2DP_OFFLOAD_STOP_V2:
         log::verbose("VS_HCI_STOP_A2DP_MEDIA successful");
+        l2c_link_set_br_coex_buf_cap(0, base::BindOnce([](bool success) {
+          if (!success) {
+            log::error("failed resetting coex buffer cap");
+          }
+        }));
         break;
       case VS_HCI_A2DP_OFFLOAD_START:
       case VS_HCI_A2DP_OFFLOAD_START_V2:
@@ -3077,15 +3083,38 @@ static void offload_vendor_callback(tBTM_VSC_CMPL* param) {
     }
   } else {
     log::verbose("Offload failed for subopcode= {}", sub_opcode);
+    if (param->opcode == VS_HCI_A2DP_OFFLOAD_ENABLE_COEX)
+      return;
     if (param->opcode != VS_HCI_A2DP_OFFLOAD_STOP && param->opcode != VS_HCI_A2DP_OFFLOAD_STOP_V2) {
       bta_av_cb.offload_start_pending_hndl = BTA_AV_INVALID_HANDLE;
       (*bta_av_cb.p_cback)(BTA_AV_OFFLOAD_START_RSP_EVT, &value);
+    } else {
+      l2c_link_set_br_coex_buf_cap(0, base::BindOnce([](bool success) {
+        if (!success) {
+          log::error("failed resetting coex buffer cap");
+        }
+      }));
     }
   }
 }
 
+/*
+ * How many ACL buffers should be reserved for A2DP offload
+ * Recommend ~30–40% of total controller ACL buffers (e.g. 8 total -> 2–3).
+ */
+#define COEX_BUFFER_COUNT_MIN 0
+#define COEX_BUFFER_COUNT_MAX 10
+#define COEX_BUFFER_COUNT_DEFAULT 5
+static uint16_t bta_av_get_coex_buffer_count() {
+  int val = osi_property_get_int32(
+      "persist.bluetooth.a2dp_offload.coex_buf_count",
+      COEX_BUFFER_COUNT_DEFAULT);
+  val = std::clamp(val, COEX_BUFFER_COUNT_MIN, COEX_BUFFER_COUNT_MAX);
+  return static_cast<uint16_t>(val);
+}
+
 static void bta_av_vendor_offload_start(tBTA_AV_SCB* p_scb, tBT_A2DP_OFFLOAD* offload_start) {
-  uint8_t param[sizeof(tBT_A2DP_OFFLOAD)];
+  uint8_t* param = reinterpret_cast<uint8_t*>(osi_malloc(sizeof(tBT_A2DP_OFFLOAD)));
   log::verbose("");
 
   uint8_t* p_param = param;
@@ -3111,8 +3140,21 @@ static void bta_av_vendor_offload_start(tBTA_AV_SCB* p_scb, tBT_A2DP_OFFLOAD* of
           offload_start->codec_type, offload_start->sample_rate, offload_start->bits_per_sample,
           offload_start->ch_mode, offload_start->encoded_audio_bitrate, offload_start->acl_hdl,
           offload_start->l2c_rcid, offload_start->mtu);
-  get_btm_client_interface().vendor.BTM_VendorSpecificCommand(HCI_CONTROLLER_A2DP, p_param - param,
-                                                              param, offload_vendor_callback);
+  uint16_t coex_buf_count = bta_av_get_coex_buffer_count();
+  l2c_link_set_br_coex_buf_cap(coex_buf_count, base::BindOnce([](uint8_t* param, uint8_t* p_param,
+                                                                 uint16_t coex_buf_count, bool success) {
+    if (!success) {
+      log::error("failed setting coex buffer cap");
+    }
+    if (osi_property_get_bool("persist.bluetooth.a2dp_offload.mtk_coex", false)) {
+      uint8_t mtk_param[] = {VS_HCI_A2DP_OFFLOAD_ENABLE_COEX, static_cast<uint8_t>(coex_buf_count)};
+      get_btm_client_interface().vendor.BTM_VendorSpecificCommand(HCI_CONTROLLER_A2DP, sizeof(mtk_param),
+                                                                  mtk_param, offload_vendor_callback);
+    }
+    get_btm_client_interface().vendor.BTM_VendorSpecificCommand(HCI_CONTROLLER_A2DP, p_param - param,
+                                                                param, offload_vendor_callback);
+    osi_free(param);
+  }, param, p_param, coex_buf_count));
 }
 
 static void bta_av_vendor_offload_start_v2(tBTA_AV_SCB* p_scb, A2dpCodecConfigExt* offload_codec) {

PixelOS-AOSP/android_packages_modules_Bluetooth

system/bta/av/bta_av_aact.cc
@@ -77,6 +77,7 @@
 #include "stack/include/btm_status.h"
 #include "stack/include/hci_error_code.h"
 #include "stack/include/hcidefs.h"
+#include "stack/include/l2cap_av_interface.h"
 #include "stack/include/l2cap_interface.h"
 #include "stack/include/l2cap_types.h"
 #include "stack/include/sdpdefs.h"
@@ -3012,6 +3013,13 @@ void bta_av_open_at_inc(tBTA_AV_SCB* p_scb, tBTA_AV_DATA* p_data) {
   }
 }
 
+// Default A2DP MTK offload coex opcode
+constexpr uint8_t kA2dpCoexOpcodeDefault = 0xA0;
+static uint8_t bta_av_get_coex_opcode(void) {
+  return osi_property_get_int32("persist.bluetooth.a2dp_offload.coex_opcode",
+            kA2dpCoexOpcodeDefault);
+}
+
 static void offload_vendor_callback(tBTM_VSC_CMPL* param) {
   tBTA_AV value{0};
   uint8_t sub_opcode = 0;
@@ -3026,6 +3034,11 @@ static void offload_vendor_callback(tBTM_VSC_CMPL* param) {
       case VS_HCI_A2DP_OFFLOAD_STOP:
       case VS_HCI_A2DP_OFFLOAD_STOP_V2:
         log::debug("VS_HCI_STOP_A2DP_MEDIA successful");
+        l2c_link_set_br_coex_buf_cap(0, base::BindOnce([](bool success) {
+          if (!success) {
+            log::error("failed resetting coex buffer cap");
+          }
+        }));
         break;
       case VS_HCI_A2DP_OFFLOAD_START:
       case VS_HCI_A2DP_OFFLOAD_START_V2:
@@ -3043,15 +3056,29 @@ static void offload_vendor_callback(tBTM_VSC_CMPL* param) {
     }
   } else {
     log::debug("Offload failed for subopcode= {}", sub_opcode);
+    if (param->opcode == bta_av_get_coex_opcode()) {
+      return;
+    }
     if (param->opcode != VS_HCI_A2DP_OFFLOAD_STOP && param->opcode != VS_HCI_A2DP_OFFLOAD_STOP_V2) {
       bta_av_cb.offload_start_pending_hndl = BTA_AV_INVALID_HANDLE;
       (*bta_av_cb.p_cback)(BTA_AV_OFFLOAD_START_RSP_EVT, &value);
+    } else {
+      l2c_link_set_br_coex_buf_cap(0, base::BindOnce([](bool success) {
+        if (!success) {
+          log::error("failed resetting coex buffer cap");
+        }
+      }));
     }
   }
 }
 
+// Number of ACL buffers reserved for A2DP offload coex
+static uint8_t bta_av_get_coex_buffer_count(void) {
+  return osi_property_get_int32("persist.bluetooth.a2dp_offload.coex_buf_count", 0);
+}
+
 static void bta_av_vendor_offload_start(tBTA_AV_SCB* p_scb, tBT_A2DP_OFFLOAD* offload_start) {
-  uint8_t param[sizeof(tBT_A2DP_OFFLOAD)];
+  uint8_t* param = reinterpret_cast<uint8_t*>(osi_malloc(sizeof(tBT_A2DP_OFFLOAD)));
   log::debug("");
 
   uint8_t* p_param = param;
@@ -3077,8 +3104,21 @@ static void bta_av_vendor_offload_start(tBTA_AV_SCB* p_scb, tBT_A2DP_OFFLOAD* of
           offload_start->codec_type, offload_start->sample_rate, offload_start->bits_per_sample,
           offload_start->ch_mode, offload_start->encoded_audio_bitrate, offload_start->acl_hdl,
           offload_start->l2c_rcid, offload_start->mtu);
-  get_btm_client_interface().vendor.BTM_VendorSpecificCommand(HCI_CONTROLLER_A2DP, p_param - param,
-                                                              param, offload_vendor_callback);
+  uint8_t coex_buf_count = bta_av_get_coex_buffer_count();
+  l2c_link_set_br_coex_buf_cap(coex_buf_count, base::BindOnce([](uint8_t* param, uint8_t* p_param,
+                                                                 uint8_t coex_buf_count, bool success) {
+    if (!success) {
+      log::error("failed setting coex buffer cap");
+    }
+    if (coex_buf_count > 0) {
+      uint8_t mtk_param[] = {bta_av_get_coex_opcode(), coex_buf_count};
+      get_btm_client_interface().vendor.BTM_VendorSpecificCommand(HCI_CONTROLLER_A2DP, sizeof(mtk_param),
+                                                                  mtk_param, offload_vendor_callback);
+    }
+    get_btm_client_interface().vendor.BTM_VendorSpecificCommand(HCI_CONTROLLER_A2DP, p_param - param,
+                                                                param, offload_vendor_callback);
+    osi_free(param);
+  }, param, p_param, coex_buf_count));
 }
 
 static void bta_av_vendor_offload_start_v2(tBTA_AV_SCB* p_scb, A2dpCodecConfigExt* offload_codec) {